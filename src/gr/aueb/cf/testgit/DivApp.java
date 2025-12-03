b1f5e8b0db4f7de2c655ab436fbdf2cb43c8687b
Nikolaos Kolios <nikolaos.kolios@mitel.com>
2025-12-03 14:04:19 +0200

    added DivApp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class DivApp {
+    static void main() {
+        System.out.println("DivApp running...");
+        System.out.println(div(4,2));
+    }
+
+    static int div(int a, int b) {
+        return a / b;
+    }
+}
