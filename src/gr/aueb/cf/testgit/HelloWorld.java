1bf5ab6d436e75442cf048fec6df66602324f441
Nikolaos Kolios <nikolaos.kolios@mitel.com>
2025-12-03 15:22:42 +0200

    added to print

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.testgit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!");
 
     }
 }
